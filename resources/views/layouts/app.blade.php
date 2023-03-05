3de1b771fe5c0734a1f61a99e96a64b060fb1a33
Jon Zenor <JonZenor@gmail.com>
2023-03-05 11:53:20 -0700

    Closes #1 Basic layout is complete

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,8 @@
                 <label tabindex="0" class="btn btn-ghost lg:hidden">
                   <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                 </label>
-                <ul tabindex="0" class="p-2 mt-3 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
+
+                {{-- <ul tabindex="0" class="p-2 mt-3 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
                   <li><a>Item 1</a></li>
                   <li tabindex="0">
                     <a class="justify-between">
@@ -37,7 +38,7 @@
                     </ul>
                   </li>
                   <li><a>Item 3</a></li>
-                </ul>
+                </ul> --}}
               </div>
 
               {{-- Left hand logo items --}}
@@ -46,7 +47,7 @@
 
             {{-- Normal desktop nav menu --}}
             <div class="hidden navbar-center lg:flex">
-              <ul class="px-1 menu menu-horizontal bg-neutral">
+              {{-- <ul class="px-1 menu menu-horizontal bg-neutral">
                 <li><a>Item 1</a></li>
                 <li tabindex="0">
                   <a>
@@ -59,7 +60,7 @@
                   </ul>
                 </li>
                 <li><a>Item 3</a></li>
-              </ul>
+              </ul> --}}
             </div>
 
             {{-- Right hand nav elements for all sizes--}}
